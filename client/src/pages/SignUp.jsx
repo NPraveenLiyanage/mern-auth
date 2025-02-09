fe3d5de9c9498cb3dfd992aa7c552a9cf2d0c48a
NPraveenLiyanage <nipunaliyange44@gmail.com>
2025-02-09 12:23:23 +0530

    complete signIn page functionallity

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     //....use strip operator to keep the previous value of the form data
@@ -30,6 +31,7 @@ export default function SignUp() {
         setError(true);
         return;
       }
+      navigate('/sign-in');
     }catch (error) {
       setLoading(false);
       setError(true);
